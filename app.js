9481487cb23486d7b8451956b31403e2e93aa63b
HopeFourie <hopefourie@gmail.com>
2020-10-01 16:01:16 -0400

    models synced

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const layout = require('./views/layout')
+const { db, Page, User } = require('./models');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req,res, next)=>{
-  res.send('Hello World')
+  res.send(layout(''))
 })
 
-app.listen(3000, ()=>{
-  console.log('Listening on http://localhost:3000')
-})
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
+
+const init = async () => {
+  await db.sync();
+  app.listen(3000, ()=>{
+    console.log('Listening on http://localhost:3000')
+  })
+}
+
+init()
