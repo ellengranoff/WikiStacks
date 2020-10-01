a73677e42622649f03f40909ceddb3f693ff1ab7
HopeFourie <hopefourie@gmail.com>
2020-10-01 15:06:25 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req,res, next)=>{
+  res.send('Hello World')
+})
+
+app.listen(3000, ()=>{
+  console.log('Listening on http://localhost:3000')
+})
